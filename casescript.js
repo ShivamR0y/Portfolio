a87d4d0c37ecfe342b6a3268c511bdaf4619370d
ShivamR0y <toyroyshibu@gmail.com>
2025-01-08 11:16:13 +0530

    COnnected navbar sections for 99acres

--- casescript.js
+++ casescript.js
@@ -244,8 +244,8 @@ function navHighlighter() {
   
   // Now we loop through sections to get height, top and ID values for each
   sections.forEach(current => {
-    const sectionHeight = current.offsetHeight;
-    const sectionTop = current.offsetTop - 50;
+    const sectionHeight = current.offsetHeight+100;
+    const sectionTop = current.offsetTop+200;
     sectionId = current.getAttribute("id");
     
     /*
